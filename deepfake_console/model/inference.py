5bd161831f4482a1a10598d75764d65079155d06
abhinav-ghosh <abhinavchungath@gmail.com>
2025-11-07 19:29:23 +0530

    inference.py

--- deepfake_console/model/inference.py
+++ deepfake_console/model/inference.py
@@ -1,22 +1,22 @@
-import tempfile
-import os
-from model.deepfake2_model import predict_video
-from model.face_extractor import extract_face_video
+import tempfile      # to create temporary files
+import os            # for file path operations
+from model.deepfake2_model import predict_video  # function to predict using the model
+from model.face_extractor import extract_face_video  # function to extract face frames
 
 def analyze_video(video_file):
-    checkpoint_path = "model/weights/best_resnext_lstm.pth"  # or your actual .pth
+    checkpoint_path = "model/weights/best_resnext_lstm.pth"  # weight is saved here
 
-    # Save uploaded video
+    # uploaded video is saved temporarily for further processing
     with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as tmp:
         tmp.write(video_file.read())
         tmp_path = tmp.name
 
-    # Extract face frames (folder path)
+    # uses the video saved temporarily to extract face frames
     face_frames_folder = extract_face_video(tmp_path, num_frames=60)
     if face_frames_folder is None or not os.listdir(face_frames_folder):
         return {"error": "Face extraction failed — no faces detected."}
 
-    # Predict
+    # uses the model to predict on the extracted face frames
     result = predict_video(face_frames_folder, checkpoint_path, threshold=0.5)
 
     if "error" in result:
